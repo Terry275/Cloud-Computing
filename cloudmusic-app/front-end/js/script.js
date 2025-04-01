94889b6e25c1a7ee895c546db72815ea53578883
Truong Tuong Hao <s3979259@rmit.edu.vn>
2025-04-01 23:05:53 +1100

    Fixing the register page field

--- cloudmusic-app/front-end/js/script.js
+++ cloudmusic-app/front-end/js/script.js
@@ -20,6 +20,10 @@ function showLogin() {
       <h3 class="text-center">Register</h3>
       <form class="w-50 mx-auto">
         <div class="mb-3">
+          <label for="register-email" class="form-label">Email address</label>
+          <input type="email" class="form-control" id="register-email" required>
+        </div>
+        <div class="mb-3">
           <label for="register-username" class="form-label">Username</label>
           <input type="text" class="form-control" id="register-username" required>
         </div>
@@ -32,6 +36,7 @@ function showLogin() {
     `;
   }
   
+  
   function login() {
     const username = document.getElementById("login-username").value;
     alert(`Welcome back, ${username}`);
@@ -40,8 +45,11 @@ function showLogin() {
   }
   
   function register() {
+    const email = document.getElementById("register-email").value;
     const username = document.getElementById("register-username").value;
-    alert(`Thanks for registering, ${username}`);
+    const password = document.getElementById("register-password").value;
+  
+    alert(`Registered with:\nEmail: ${email}\nUsername: ${username}`);
     document.getElementById("main-content").innerHTML = `<h3 class="text-primary text-center">Registration complete!</h3>`;
   }
   
