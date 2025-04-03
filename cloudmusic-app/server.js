4f276107c69cbb210615fc4cb45da9ded03443d3
Terry275 <ab1079bbdac3287e@gmail.com>
2025-04-04 02:34:28 +1100

    Add fetch() logic for Login

---
+++ cloudmusic-app/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// 1. Serve all static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 2. Define the root route to serve login.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+
+
+// 3. Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
